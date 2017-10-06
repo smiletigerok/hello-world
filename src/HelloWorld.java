9585f61e1caa582cd120e3a24b83fa132ba567b3
Administrator <Administrator@DESKTOP-AS96FL8>
2017-10-07 01:43:51 +0900

    hi world 메시지 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World OK 77");
 		System.out.println("Hello World OK 88");
+		System.out.println("Good");
 	}
 }
